6b4cbd33b0cce1c6999e46147ad7c53815f5a235
iamkrz007 <iamkrz007@gmail.com>
2021-11-18 20:27:47 +0530

    app changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1>Hello from node.js</h1></body>');
+    res.write('<body><h1>Hello from node.js again </h1></body>');
     res.write('</html>');
     res.end();
 });
