fc7763e258ae0752030777fe445c7a3aaa97c2ec
iamkrz007 <iamkrz007@gmail.com>
2021-11-19 21:08:07 +0530

    message

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);\
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Mesage</title></head>');
+        res.write('<body><form action ="/message" method="POST" ><input type = "text" name="message"><button type="submit"> Send </button></input></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === "POST"){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode= 302;
+        res.setHeader('Location', '/');
+        return res.end();
+
+    }
     // process.exit();
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
