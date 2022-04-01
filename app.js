f4ec26b2f21732b80e37fae0fc8fab1cd8e6f1de
iamkrz007 <iamkrz007@gmail.com>
2022-04-02 01:51:31 +0530

    2nd april

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
+const path = require('path');
+
 const express = require('express');
-//const { use } = require('express/lib/application');
-const bodyParser= require('body-parser');
+ 
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/admin',adminRoutes);
 
-app.use('/',(req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
-
-app.use('/add-products', (req, res, next) => {
-    console.log('in the add product middleware');
-    res.send('<form action="/product" method="POST" ><input type="text" name="title"><button type="submit">Add Product </button> </form>');
-    // next(); //allows the request to the next middleware
-});
-app.post('/product',(req,res,next) =>{
-    console.log('in the product middleware');
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    console.log('in another middleware');
-    res.send('<h1>hello from express</h1>')
-    // next(); //allows the request to the next middleware
-});
+    res.status(404).sendFile(path.join(__dirname, 'views' , 'page-not-found.html'));
+ });
 
 app.listen(3000);
