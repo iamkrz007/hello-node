8ac473f03e27b2ccae7c96ba2d0a5755324cd679
iamkrz007 <iamkrz007@gmail.com>
2024-07-21 08:22:50 +0530

    add test 2

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const shopRoutes = require('./routes/shop');
 
 const bodyParser = require('body-parser');
 
-
-
-
-
-
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,5 +20,4 @@ app.use('/', (req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views' , 'page-not-found.html'));
  });
 
-// this is main-dev chnage
 app.listen(3000);
